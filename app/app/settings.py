fab2ba73c28c1d90232db4bbbc32ce09a70b1ea6
ShengzhenFu <fushengzhen@163.com>
2023-09-09 15:55:34 +0800

    implement user API

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l*srroa8a-bb=ib8o&0-$vk6340!xv#pm7n(pgsz7(4969h0#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.221', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,6 +17,15 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
@@ -33,7 +42,7 @@ class AuthTokenSerializer(serializers.Serializer):
         user = authenticate(
             request=self.context.get('request'),
             username=email,
-            password=password,
+            password=password
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
